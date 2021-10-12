86f1be1370e1857eff2e1119388f96e48c812312
Sarah Schwartz <sarah.schwartz93@gmail.com>
2021-10-12 06:47:06 -0700

    started separating into modules

---
+++ examples/my_ar/jsm/add-tools.js
@@ -0,0 +1,35 @@
+function addTools(engine, position, scale, color) {
+
+    const cubeButton = document.getElementById('cube-button');
+    const sphereButton = document.getElementById('sphere-button');
+    const cylinderButton = document.getElementById('cylinder-button');
+    const planeButton = document.getElementById('plane-button');
+
+        // Add a cube
+    const addCube = () => {
+        engine.addBox(position, scale, color);
+    }
+
+    // Add a sphere
+    const addSphere = () => {
+        engine.addSphere(position, scale, color);
+    }
+
+    // Add a cylinder
+    const addCylinder = () => {
+        engine.addCylinder(position, scale, color);
+    }
+
+    // Add a plane
+    const addPlane = () => {
+        engine.addPlane(position, scale, color);
+    }
+
+    cubeButton.addEventListener("click", addCube)
+    sphereButton.addEventListener("click", addSphere)
+    cylinderButton.addEventListener("click", addCylinder)
+    planeButton.addEventListener("click", addPlane)
+    
+}
+
+export { addTools };
