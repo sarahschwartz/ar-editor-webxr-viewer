82024451995c1f3a429b15b4438c11a90a8e44d5
Sarah Schwartz <sarah.schwartz93@gmail.com>
2021-10-13 16:05:59 -0700

    scale on scale tool select

--- examples/my_ar/jsm/render.js
+++ examples/my_ar/jsm/render.js
@@ -2,35 +2,65 @@ function updateScene(engine, currentObjectIndex, currentEditTool, debug) {
     let last = currentObjectIndex
     const editToolbar = document.getElementById('edit-toolbar');
 
+    let scaleIncrement = 0.01;
+    let scaleMax = 3;
+    let scaleX, scaleY, scaleZ;
+
+    if (engine._root.children[last].scale.x < scaleMax) {
+        scaleX = engine._root.children[last].scale.x + scaleIncrement
+        scaleY = engine._root.children[last].scale.y + scaleIncrement
+        scaleZ = engine._root.children[last].scale.z + scaleIncrement
+        
+    }
+
 
     // If Edit toolbar is active
     if (editToolbar.classList.contains("active-toolbar")) {
-        switch (engine._root.children[last].geometry.type) {
-            case "CylinderGeometry":
-                debug.innerHTML = "Cylinder";
-                engine._root.children[last].scale.set(2, 2, 2);
-                engine._root.children[last].position.set(-0.5, 0, -0.5);
-                debug.innerHTML += " Scale: " + JSON.stringify(engine._root.children[last].scale);
+        switch (currentEditTool) {
+            case "scale":
+                debug.innerHTML = "Scale tool <br>";
+                engine._root.children[last].scale.set(scaleX, scaleY, scaleZ);
                 break;
-            case "BoxBufferGeometry":
-                debug.innerHTML = "Box";
-                debug.innerHTML += " Scale: " + JSON.stringify(engine._root.children[last].scale);
-                debug.innerHTML += " Width: " + engine._root.children[last].geometry.parameters.width;
-                debug.innerHTML += " Height: " + engine._root.children[last].geometry.parameters.height;
-                debug.innerHTML += " Depth: " + engine._root.children[last].geometry.parameters.depth;
+            case "rotate":
+                debug.innerHTML = "Rotate tool <br>";
                 break;
-            case "SphereBufferGeometry":
-                debug.innerHTML = "Sphere";
-                debug.innerHTML += " Radius: " + engine._root.children[last].geometry.parameters.radius;
+            case "color":
+                debug.innerHTML = "Color tool <br>";
                 break;
-            case "PlaneGeometry":
-                debug.innerHTML = "Plane";
-                debug.innerHTML += " Width: " + engine._root.children[last].geometry.parameters.width;
-                debug.innerHTML += " Height: " + engine._root.children[last].geometry.parameters.height;
+            case "move":
+                debug.innerHTML = "Move tool <br>";
                 break;
             default:
-                debug.innerHTML = "Geometry not found "
+                debug.innerHTML = "no tool selected"
         }
+
+
+        // switch (engine._root.children[last].geometry.type) {
+        //     case "CylinderGeometry":
+        //         debug.innerHTML = "Cylinder";
+        //         engine._root.children[last].scale.set(2, 2, 2);
+        //         engine._root.children[last].position.set(-0.5, 0, -0.5);
+        //         debug.innerHTML += " Scale: " + JSON.stringify(engine._root.children[last].scale);
+        //         break;
+        //     case "BoxBufferGeometry":
+        //         debug.innerHTML = "Box";
+        //         debug.innerHTML += " Scale: " + JSON.stringify(engine._root.children[last].scale);
+        //         debug.innerHTML += " Width: " + engine._root.children[last].geometry.parameters.width;
+        //         debug.innerHTML += " Height: " + engine._root.children[last].geometry.parameters.height;
+        //         debug.innerHTML += " Depth: " + engine._root.children[last].geometry.parameters.depth;
+        //         break;
+        //     case "SphereBufferGeometry":
+        //         debug.innerHTML = "Sphere";
+        //         debug.innerHTML += " Radius: " + engine._root.children[last].geometry.parameters.radius;
+        //         break;
+        //     case "PlaneGeometry":
+        //         debug.innerHTML = "Plane";
+        //         debug.innerHTML += " Width: " + engine._root.children[last].geometry.parameters.width;
+        //         debug.innerHTML += " Height: " + engine._root.children[last].geometry.parameters.height;
+        //         break;
+        //     default:
+        //         debug.innerHTML = "Geometry not found "
+        // }
         debug.innerHTML += "Current edit tool: " + currentEditTool
     }
 }
